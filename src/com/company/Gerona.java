46d9f41f4f8167b5e0391422f4a43222e6fd7417
Ailes2 <fb.smirnov.u@gmail.com>
2021-08-02 15:52:02 +0300

    Initial commit

---
+++ src/com/company/Gerona.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Gerona {
+    public static void main(String[] args) {
+        System.out.printf("Площадь %.2f",geroneSquare(14, 12, 12));}
+
+
+           public static double geroneSquare(double a, double b, double c) {
+                double p = (a + b + c) / 2;
+                return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
---
+++ src/com/company/Pythagoras.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Pythagoras {
+    public static void main(String[] args) {
+        int a=40,b=45;
+      System.out.println("Площадь прямоугольного теругольника с катетами " + a+" и "+ b+ " равна " +(a*b)/2);
+      double c=Math.sqrt(a*a+b*b);
+      System.out.println("Периметр прямоугольного теругольника с катетами " + a+" и "+ b+ " и гипотенузой " + (float) c+" равен " +(float)(a+b+c));
+    }
+}
